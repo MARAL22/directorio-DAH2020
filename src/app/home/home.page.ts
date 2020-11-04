335af1e95a7995ffd13e646f174ef1220642857c
Manuel <jose.ma97.jmm@gmail.com>
2020-11-03 22:55:54 -0700

    Base template for list view

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,18 @@
 import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
+
+
+
+public users = [];
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-  constructor(private data: DataService) {}
+  constructor() {}
 
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
-  }
-
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
+  
 
 }
